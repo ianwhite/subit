d378b3635a976dafbc34824fe9276d2316070180
Ian White <ian.w.white@gmail.com>
2009-07-10 13:17:56 +0100

    Added specs and code for Subit.logger

--- lib/subit.rb
+++ lib/subit.rb
@@ -1,10 +1,21 @@
+require 'set'
+require 'logger'
+require 'subit/rule'
 require 'subit/rules'
 require 'subit/version'
 
 module Subit
   extend self
-  
+
   def rules(*args, &block)
     Rules.new(*args, &block)
+  end
+  
+  def logger
+    @logger ||= (Rails.logger rescue nil) || Logger.new(STDERR)
+  end
+
+  def logger=(logger)
+    @logger = logger
   end
 end
--- spec/subit_spec.rb
+++ spec/subit_spec.rb
@@ -8,4 +8,36 @@ describe Subit do
   it ".rules should return a Subit::Rules object" do
     Subit.rules.should be_instance_of(Subit::Rules)
   end
+  
+  describe ".logger" do
+    before do
+      Rails = mock("Rails") unless defined?(Rails)
+    end
+    
+    after do
+      Subit.logger = nil
+      Object.instance_eval { remove_const :Rails } if Rails.is_a?(mock.class)
+    end
+    
+    it "should use Rails.logger if available" do
+      Rails.stub(:logger).and_return(rails_logger = mock)
+      Subit.logger.should == rails_logger
+    end
+
+    it "should create new Logger on STDERR if Rails.logger not available" do
+      Rails.stub(:logger).and_raise(NoMethodError)
+      Logger.should_receive(:new).with(STDERR).and_return(new_logger = mock)
+      Subit.logger.should == new_logger
+    end
+    
+    it "should allow setting logger" do
+      Subit.logger = :foo
+      Subit.logger.should == :foo
+    end
+    
+    it "should cache logger" do
+      Subit.logger = Object.new
+      Subit.logger.object_id.should == Subit.logger.object_id
+    end
+  end
 end
