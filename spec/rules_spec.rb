477451999b45119f547dbf532f53005b4eec0e0f
Ian White <ian.w.white@gmail.com>
2009-07-12 11:16:22 +0100

    More specs for Subit::Rules object

--- spec/rules_spec.rb
+++ spec/rules_spec.rb
@@ -1,6 +1,44 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe Subit::Rules do
+  before do
+    @rules = Subit::Rules.new
+  end
+  
+  describe "#method_missing" do
+    it "forwards rule symbols to #add" do
+      @rules.should_receive(:rule_class).with(:foo).and_return(true)
+      @rules.should_receive(:add).with(:foo, :bar)
+      @rules.foo :bar
+    end
+  
+    it "forwards NON rule symbols to super #method_missing" do
+      @rules.should_receive(:rule_class).with(:foo).and_return(false)
+      @rules.should_not_receive(:add)
+      lambda { @rules.foo :bar }.should raise_error(NoMethodError)
+    end
+  end
+  
+  describe "#add" do
+    it "when first arg is a rule (determined by #rule?), concat to #rules" do
+      @rules.should_receive(:rule?).with(rule = mock).and_return(true)
+      @rules.rules.should_receive(:<<).with(rule)
+      @rules.add rule
+    end
+    
+    it "when first arg has a rule_class, create using klass, and remaining args, concat to #rules" do
+      @rules.should_receive(:rule_class).with(:foo).and_return(rule_class = mock)
+      rule_class.should_receive(:new).with(:second, :third).and_return(rule = mock)
+      @rules.rules.should_receive(:<<).with(rule)
+      @rules.add :foo, :second, :third
+    end
+    
+    it "when first arg has no associated rule_class, raise an ArgumentError" do
+      @rules.should_receive(:rule_class).with(:foo).and_return(false)
+      lambda { @rules.add :foo }.should raise_error(ArgumentError)
+    end
+  end
+  
   describe "Use case" do
     before do
       @content = 'hello & goodbye'
