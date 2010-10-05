faabda265dadd23d282ce7812beefc8fd240aa7e
Ian White <ian.w.white@gmail.com>
2010-10-05 10:04:59 +0100

    Simplify spec setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,6 @@
-ENV["RAILS_ENV"] ||= "test"
-__DIR__ = File.dirname(__FILE__)
-
-# if we're in a rails env, use that, otherwise use rubygems to create a spec env
-begin
-  require "#{__DIR__}/../../../../config/environment"
-rescue LoadError
-  require 'rubygems'
-  require 'active_support'
-  $LOAD_PATH << "#{__DIR__}/../lib"
-end
-
+require 'rubygems'
+require 'active_support'
 require 'spec'
+
+$:.unshift File.dirname(__FILE__) + '/../lib'
 require 'subit'
